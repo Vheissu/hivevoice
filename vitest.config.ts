23926b480d0c1f806747b36d7272743db05d5fd9
Dwayne Charrington <dwaynecharrington@gmail.com>
2025-08-06 07:55:55 +1000

    chore(test): exclude irrelevant files from vitest coverage
    Prevents test runs from including dependencies, build artifacts, configuration files, and unrelated test directories to improve performance and accuracy of test results.

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,14 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     setupFiles: './test/setup.ts',
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+      'ui/test/**'
+    ],
   },
 });
 
